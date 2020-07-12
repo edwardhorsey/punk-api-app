51e3a6543f306b2ed1990b3c4107d8a4a1e3cc1c
Edward Horsey <edwardhorsey@me.com>
2020-07-12 09:40:56 +0100

    styles, before responsive

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import styles from './NavBar.module.scss';
 import FiltersList from '../FiltersList';
-
+import brewdogLogo from '../../assets/logo.png';
 
 class NavBar extends Component {
   state = {  }
   render() { 
     return ( 
       <nav className={styles.navBar}>
+        <img src={brewdogLogo} alt={'Brewdog Logo'} />
+        <p>Search the Brewdog Punk API</p>
         <input className={styles.search} type="text" placeholder="Search" onInput={(event) => this.props.search(event.target.value)}/>
         <FiltersList filters={this.props.filters} />
       </nav>
