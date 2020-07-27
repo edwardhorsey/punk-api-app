aac18680242c100028e07c29978bfbd23059d161
Edward Horsey <edwardhorsey@me.com>
2020-07-27 22:04:01 +0100

    burger done

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import styles from './NavBar.module.scss';
 import FiltersList from '../FiltersList';
 import brewdogLogo from '../../assets/logo.png';
+import burger from '../../assets/hamburger-menu.png';
 
 class NavBar extends Component {
-  state = {  }
+  constructor(props) {
+    super(props)
+    this.searchRef = React.createRef()
+  }
+  state = { 
+    showing: false,
+   }
+
+  toggleFilters = () => {
+    this.setState({showing: !this.state.showing}) 
+  }
+
   render() { 
     return ( 
       <nav className={styles.navBar}>
@@ -12,9 +24,12 @@ class NavBar extends Component {
           <img src={brewdogLogo} alt={'Brewdog Logo'} />
           <p>Search the Brewdog Punk API</p>
         </div>
-        <div className={styles.searchFilters}>
+        <div ref={this.searchRef} className={this.state.showing? styles.searchFilters: styles.hideFilters}>
           <input className={styles.search} type="text" placeholder="Search" onInput={(event) => this.props.search(event.target.value)}/>
           <FiltersList filters={this.props.filters} />
+        </div>
+        <div className={styles.burger}>
+          <img onClick={this.toggleFilters} src={burger} alt="burger" />
         </div>
       </nav>
      );
